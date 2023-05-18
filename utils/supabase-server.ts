77d8483a645516ca40240cd9f61db050267ba2d4
tsubokura-shuhei <repeat2639@gmail.com>
2023-05-18 22:34:09 +0900

    first cmmit

---
+++ utils/supabase-server.ts
@@ -0,0 +1,9 @@
+import { headers, cookies } from "next/headers";
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import type { Database } from "../utils/database.types";
+
+export const createClient = () =>
+  createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  });
