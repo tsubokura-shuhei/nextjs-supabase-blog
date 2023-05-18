a156d745c90513a34b9a3740e195d7ff1b9a0dda
tsubokura-shuhei <repeat2639@gmail.com>
2023-05-18 23:22:44 +0900

    auth signup add

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,10 @@
 //ローディング
 const Loading = () => {
-  return(
+  return (
     <div className="flex justify-center">
       <div className="h-10 w-10 animate-spin rounded-full border-4 border-yellow-100"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
